fa462fa65deaf4e7ccb65c9ff88f41e2a8d34020
qpminh <minhphanquang01@gmail.com>
2025-10-22 21:20:25 +0700

    merge new hoang' version

--- Application/StationInventories/Queries/GetStationInventoryHandler.cs
+++ Application/StationInventories/Queries/GetStationInventoryHandler.cs
@@ -1,21 +1,24 @@
-﻿namespace Application.StationInventories.Queries
+﻿using Application.Common.Interfaces.Repositories;
+using Application.Dtos;
+using AutoMapper;
+using MediatR;
+
+namespace Application.StationInventories.Queries
 {
-    //public class GetStationInventoryHandler : IRequestHandler<GetStationInventoryQuery, StationInventoryDto>
-    //{
-    //    private readonly IStationInventoryRepository _repo;
-    //    private readonly IMapper _mapper;
-    //    public GetStationInventoryHandler(IStationInventoryRepository repo, IMapper mapper)
-    //    {
-    //        _repo = repo;
-    //        _mapper = mapper;
-    //    }
+    public class GetStationInventoryHandler : IRequestHandler<GetStationInventoryQuery, StationInventoryDto>
+    {
+        private readonly IStationInventoryRepository _repo;
+        private readonly IMapper _mapper;
+        public GetStationInventoryHandler(IStationInventoryRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
 
-    //    public async Task<StationInventoryDto> Handle(GetStationInventoryQuery request, CancellationToken ct)
-    //    {
-    //        //var station = await _repo.GetInventory(request.StationId, ct);
-    //        //return _mapper.Map<StationInventoryDto>(station);
-    //        return null;
-
-    //    }
-    //}
+        public async Task<StationInventoryDto> Handle(GetStationInventoryQuery request, CancellationToken ct)
+        {
+            var station = await _repo.GetInventory(request.StationId, ct);
+            return _mapper.Map<StationInventoryDto>(station);
+        }
+    }
 }
--- Application/SwapTransactions/Queries/GetFullSwapTransactionIDQuery.cs
+++ Application/SwapTransactions/Queries/GetFullSwapTransactionIDQuery.cs
@@ -1,21 +1,26 @@
-﻿namespace Application.SwapTransactions.Queries
+﻿using Application.Common.Interfaces.Repositories;
+using Application.Dtos;
+using AutoMapper;
+using MediatR;
+
+namespace Application.SwapTransactions.Queries
 {
-    //public record GetFullSwapTransactionIDQuery(int id) : IRequest<SwapTranscationFullDto> { }
+    public record GetFullSwapTransactionIDQuery(int id) : IRequest<SwapTranscationFullDto> { }
 
-    //public class GetFullSwapTransactionIDQueryHandler : IRequestHandler<GetFullSwapTransactionIDQuery, SwapTranscationFullDto>
-    //{
-    //    private readonly ISwapTransactionService _repo;
-    //    private readonly IMapper _mapper;
-    //    public GetFullSwapTransactionIDQueryHandler(ISwapTransactionService repo, IMapper mapper)
-    //    {
-    //        _repo = repo;
-    //        _mapper = mapper;
-    //    }
+    public class GetFullSwapTransactionIDQueryHandler : IRequestHandler<GetFullSwapTransactionIDQuery, SwapTranscationFullDto>
+    {
+        private readonly ISwapTransactionRepository _repo;
+        private readonly IMapper _mapper;
+        public GetFullSwapTransactionIDQueryHandler(ISwapTransactionRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
 
-    //    public async Task<SwapTranscationFullDto> Handle(GetFullSwapTransactionIDQuery request, CancellationToken cancellationToken)
-    //    {
-    //        var swapTransactions = await _repo.GetAllWithStationAndReversationID(request.id);
-    //        return _mapper.Map<SwapTranscationFullDto>(swapTransactions);
-    //    }
-    //}
+        public async Task<SwapTranscationFullDto> Handle(GetFullSwapTransactionIDQuery request, CancellationToken cancellationToken)
+        {
+            var swapTransactions = await _repo.GetAllWithStationAndReversationID(request.id);
+            return _mapper.Map<SwapTranscationFullDto>(swapTransactions);
+        }
+    }
 }
